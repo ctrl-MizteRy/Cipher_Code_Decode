0653bf8fdfa1ba56b97fa8cc3b9b737bfef54f53
ctrl-MizteRy <khang.ng.h.le@gmail.com>
2025-03-05 10:28:33 -0600

    updating js and css

--- front/scripts.js
+++ front/scripts.js
@@ -3,22 +3,36 @@ const cipherType = document.getElementById("cipher-type");
 const messageType = document.getElementById("code-or-decode");
 const submitMsg = document.getElementById("submit-msg");
 const msg = document.getElementById("text-msg");
-const encodeKey = document.getElementById("encode-key-container");
-const decodeKey = document.getElementById("decode-key-container");
+const encriptKey = document.getElementById("key-container");
 const cipherKey = document.getElementById("cipher-key");
 const answerBox = document.getElementById("answer");
+const ciphers = ["caesar", "substitution", "fence"];
 
 function getCipherKey() {
-    if (messageType.value == "code") {
-        encodeKey.removeAttribute("hidden");
-        if (!decodeKey.hasAttribute("hidden")) {
-            decodeKey.setAttribute("hidden", true);
+    if (ciphers.includes(cipherType.value)) {
+        if (messageType.value == "code") {
+            encriptKey.removeAttribute("hidden");
+            encriptKey.innerHTML = `
+        Encript Key:
+        <input type="text" id="message-key" value="" required placeholder="Please insert your encryption key">
+`;
+        } else if (messageType.value == "decode") {
+            encriptKey.removeAttribute("hidden");
+            if (cipherType.value == "substitution") {
+                encriptKey.innerHTML = `
+        Encript Key:
+        <input type="text" id="message-key" value="" required placeholder="Please insert your decode encrypted key">
+`;
+            } else if (cipherType.value == "caesar" || cipherType.value == "fence") {
+
+                encriptKey.innerHTML = `
+        Encript Key:
+        <input type="text" id="message-key" value="" placeholder="Please insert your decode encryption key if known">
+`;
+            }
         }
     } else {
-        decodeKey.removeAttribute("hidden");
-        if (!encodeKey.hasAttribute("hidden")) {
-            encodeKey.setAttribute("hidden", true);
-        }
+        encriptKey.setAttribute("hidden", true)
     }
 }
 
@@ -26,28 +40,73 @@ codeType.addEventListener('click', getCipherKey);
 
 submitMsg.addEventListener("click", function(e) {
     e.preventDefault();
-
-
-    fetch("http://localhost:8080/process", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            text: msg.value
-        })
-    })
-        .then(response => response.json())
-        .then(data => printoutDecode(data))
-        .catch(error => console.error("Error:", error));
+    submitButtonRespone();
 });
 
+/**
+ * @param {string} msg 
+ **/
 function printoutDecode(msg) {
     let message = msg.result.split(":")[1];
     answerBox.innerHTML = `
 <h3>
     The message:
     </h3>
-<textarea rows="8" cols="75">${message}</textarea>
+<textarea id="ans-text" rows="8" cols="75">${message}</textarea>
     `;
+}
+
+
+function connectBackend(keyVal) {
+    fetch("http://localhost:8080/process", {
+        method: "POST", headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            text: msg.value,
+            key: keyVal,
+            cod: messageType.value,
+        })
+    })
+        .then(response => response.json())
+        .then(data => printoutDecode(data))
+        .catch(error => console.error("Error:", error));
+}
+
+
+/**
+ * @param {string} key
+ * @param {string} cipher_type 
+ * @returns {boolean}
+ **/
+function checkCiphperKey(cipher_type, key) {
+    switch (cipher_type) {
+        case "caesar":
+        case "fence":
+            let val = parseInt(key);
+            return !isNaN(val)
+        case "substitution":
+            return key.length == 26;
+        default:
+            return true
+    }
+}
+
+function submitButtonRespone() {
+    let cipher_type = cipherType.value;
+    let key = document.getElementById('message-key');
+    let keyVal = (key != null) ? key.value : "";
+
+    if (cipher_type == "" || messageType.value == "") {
+        alert(`Please select the type of cipher you want to use and code or decode option
+`);
+    } else if (ciphers.includes(cipher_type) &&
+        keyVal == "") {
+        alert("Please input the cipher key")
+    } else {
+
+        if (checkCiphperKey(cipher_type, keyVal)) {
+            connectBackend(keyVal)
+        } else {
+            alert(`Please input the correct format keyvalue for ${cipher_type}`)
+        }
+    }
 }
